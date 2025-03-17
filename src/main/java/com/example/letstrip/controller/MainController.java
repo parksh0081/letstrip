cc2b144fe2e4a3999b535af3a44b5143ab8b822f
Kim, Seoyeon <ssyy9166@gamil.com>
2025-03-17 10:59:32 +0900

    Initial commit

---
+++ src/main/java/com/example/letstrip/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.letstrip.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class MainController {
+	@GetMapping("/index")
+	public String index(HttpSession session, Model model) {
+	    String memId = (String) session.getAttribute("memId");
+	    model.addAttribute("memId", memId);
+	    return "/main/index";
+	}
+}
---
+++ src/test/java/com/example/letstrip/LetstripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letstrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetstripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
