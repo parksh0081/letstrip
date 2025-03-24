b7f1a16e5106423b205d5f04d775af823163e806
parksh <icecream080118@gmail.com>
2025-03-24 16:47:58 +0900

    css-ing

--- src/main/java/com/example/letstrip/controller/TimelineController.java
+++ src/main/java/com/example/letstrip/controller/TimelineController.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -22,8 +23,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.letstrip.dto.TimelineDTO;
+import com.example.letstrip.entity.Place;
+import com.example.letstrip.entity.Review;
+import com.example.letstrip.entity.Store;
 import com.example.letstrip.entity.Timeline;
 import com.example.letstrip.entity.Travelplan;
+import com.example.letstrip.service.MapReviewService;
+import com.example.letstrip.service.PlaceService;
+import com.example.letstrip.service.StoreService;
 import com.example.letstrip.service.TimelineService;
 import com.example.letstrip.service.TravelplanService;
 
@@ -36,12 +43,28 @@ public class TimelineController {
 	TimelineService timelineService;
 	@Autowired
 	TravelplanService travelplanService;
+	@Autowired
+	StoreService storeService;
+	@Autowired
+	PlaceService placeService;
+	@Autowired
+	MapReviewService mapReviewService;
 	// 메인 화면
 	@GetMapping("/planner/myPlanner")
 	public String myPlanner(Model model, HttpSession session) {
 		String personId = (String) session.getAttribute("personId");
-		model.addAttribute("personId", personId);
+		// 찜 리스트 들고오기 
+ 		List<Store>storeList=storeService.selectList(personId);
+ 		if (storeList == null) {  //storeList가 null이라면 빈 리스트로 초기화
+ 	        storeList = new ArrayList<>();
+ 	    }
+ 		for(Store store:storeList) {
+ 			store.addPlace(placeService.select(store.getPlaceid()));
+ 		}
+ 		
+ 		model.addAttribute("personId", personId);
 		model.addAttribute("req", "none");
+ 		model.addAttribute("storeList",storeList);
 		return "planner/myPlanner";
 	}
 	
@@ -61,11 +84,19 @@ public class TimelineController {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDateTime startDateTime = LocalDate.parse(startDate, formatter).atStartOfDay();
         LocalDateTime endDateTime = LocalDate.parse(endDate, formatter).atStartOfDay();
-
+	    // 찜 리스트 들고오기 
+  		List<Store>storeList=storeService.selectList(personId);
+  		if (storeList == null) {  //storeList가 null이라면 빈 리스트로 초기화
+  	        storeList = new ArrayList<>();
+  	    }
+  		for(Store store:storeList) {
+  			store.addPlace(placeService.select(store.getPlaceid()));
+  		}
         model.addAttribute("personId", personId);
         model.addAttribute("plan_name", plan_name);
         model.addAttribute("startDate", startDateTime);
         model.addAttribute("endDate", endDateTime);
+ 		model.addAttribute("storeList",storeList);
 
         model.addAttribute("req", "/planner/timeline");
         return "planner/myPlanner";
@@ -84,10 +115,19 @@ public class TimelineController {
     	System.out.println("id = " + personId);
     	List<Timeline> list = timelineService.findByIdAndName(plan_name, personId);
 
+    	// 찜 리스트 들고오기 
+ 		List<Store>storeList=storeService.selectList(personId);
+ 		if (storeList == null) {  //storeList가 null이라면 빈 리스트로 초기화
+ 	        storeList = new ArrayList<>();
+ 	    }
+ 		for(Store store:storeList) {
+ 			store.addPlace(placeService.select(store.getPlaceid()));
+ 		}
+ 		
     	model.addAttribute("personId", personId);
     	model.addAttribute("plan_name", plan_name);
     	model.addAttribute("items", list);
-    	
+    	model.addAttribute("storeList",storeList);
     	model.addAttribute("req", "/planner/timelineHistory");
         return "planner/myPlanner";
     }
@@ -124,7 +164,14 @@ public class TimelineController {
     @ResponseBody
     public String saveTimeline(Model model, TimelineDTO dto, HttpSession session) {
         String personId = (String) session.getAttribute("personId");
-
+		// 찜 리스트 들고오기 
+		List<Store>storeList=storeService.selectList(personId);
+		if (storeList == null) {  //storeList가 null이라면 빈 리스트로 초기화
+		    storeList = new ArrayList<>();
+		}
+		for(Store store:storeList) {
+			store.addPlace(placeService.select(store.getPlaceid()));
+		}
         // Travelplan 객체 생성 및 연결
         Travelplan travelplan = new Travelplan();
         travelplan.setId(personId);
@@ -141,6 +188,7 @@ public class TimelineController {
         model.addAttribute("plan_name", plan_name);
         model.addAttribute("startDate", startDate);
         model.addAttribute("endDate", endDate);
+ 		model.addAttribute("storeList",storeList);
 
         model.addAttribute("req", "/planner/timeline");
         return "planner/myPlanner";
@@ -182,5 +230,21 @@ public class TimelineController {
     		model.addAttribute("req", "none");
             return "planner/myPlanner";
         }
+    
+    // wishlistdetail
+ 	@GetMapping("/planner/wishlistDetail")
+ 	public String wishlistDetail(Model model, HttpSession session, @RequestParam("place_id") String place_id) {
+        String personId = (String) session.getAttribute("personId");
+        model.addAttribute("personId", personId);
+ 		// 장소 정보 제공을 위한 
+		Place place=placeService.select(place_id);
+		// 별점 평균
+		double finalStar=mapReviewService.selectAvgStar(place_id);
+
+		model.addAttribute("place", place);
+		model.addAttribute("place_id", place_id);
+		model.addAttribute("finalStar", finalStar);
+ 		return "planner/wishlist :: wishDetailContainer";
+ 	}
     
 }
--- src/main/java/com/example/letstrip/controller/TravelplanController.java
+++ src/main/java/com/example/letstrip/controller/TravelplanController.java
@@ -9,8 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.letstrip.dto.TravelplanDTO;
+import com.example.letstrip.entity.Store;
 import com.example.letstrip.entity.Timeline;
 import com.example.letstrip.entity.Travelplan;
+import com.example.letstrip.service.PlaceService;
+import com.example.letstrip.service.StoreService;
 import com.example.letstrip.service.TravelplanService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,15 +23,26 @@ import jakarta.servlet.http.HttpSession;
 public class TravelplanController {
 	@Autowired
 	TravelplanService service;
+	@Autowired
+	StoreService storeService;
+	@Autowired
+	PlaceService placeService;
 	
 	@GetMapping("/planner/travelplan")
 	public String travelplan(Model model, HttpServletRequest request, HttpSession session) {
 		String personId = (String) session.getAttribute("personId");
 		TravelplanDTO dto = new TravelplanDTO();
 		dto.setId(personId);
-		
+		// 찜 리스트 들고오기 
+ 		List<Store>storeList=storeService.selectList(personId);
+ 		if (storeList == null) {  //storeList가 null이라면 빈 리스트로 초기화
+ 	        storeList = new ArrayList<>();
+ 	    }
+ 		for(Store store:storeList) {
+ 			store.addPlace(placeService.select(store.getPlaceid()));
+ 		}
 		service.createTravelPlan(personId);
-
+ 		model.addAttribute("storeList",storeList);
 		model.addAttribute("personId", personId);
 		model.addAttribute("req", "/planner/travelplan");
 		return "planner/myPlanner";
